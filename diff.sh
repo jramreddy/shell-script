46ffa3352075ee96e561481156b9c22594dc42f7
jramreddy <jramreddy@gmail.com>
2023-12-14 16:01:37 +0530

    new program

---
+++ diff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#substraction using the variables 
+echo "Print First Number: "
+read num1
+
+echo "Print Second Number: "
+read num2
+
+diff=$(($num2-$num1))
+
+echo "The differnce b/n the two numbers is: $diff."
+
+
+
