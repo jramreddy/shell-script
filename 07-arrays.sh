e25f35e4f272b110f1b79c2614f5392208c0e8e3
jramreddy <jramreddy@gmail.com>
2023-12-14 15:42:36 +0530

    new program

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("Bannana" "APPLE" "SAPOTA")
+
+
+echo "FIRST fruit name in the array is : ${FRUITS[0]} ."
+echo "SECOND fruit name in the array is : ${FRUITS[1]} ."
+echo "Third fruit name in the array is : ${FRUITS[2]}."
