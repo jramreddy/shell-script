6af985f3897fa8a8c22def26e95815f8787440ba
jramreddy <jramreddy@gmail.com>
2023-12-14 12:32:07 +0530

    modified the touch

--- 01.hello-world.sh
+++ 01.hello-world.sh
@@ -1,4 +1,12 @@
-#!bin/bash
+#!bin/bash   
 
+#shebang 
+
+# this is my first shell script
 echo "this is my first shell script"
+# Create 10 files using touch without opening it
+touch file{001..010}.txt
+
+
+
 
