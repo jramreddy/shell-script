4b60bebdc6d061ecd6ab93295c41f2b0a1a33fff
jramreddy <jramreddy@gmail.com>
2023-12-19 10:58:08 +0530

    pushing the changes

---
+++ conditions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# conditions 
+
+
+# if(expression) {
+#     statement if expresssion is True
+
+# }
+
+# else {
+
+#     statement if expression is False
+# }
+
+# else if(expression) {
+
+#     statement if expresssion is True
+    
+# }
+# else if (expression){
+#     statement if expresssion is True
+# }
+
+# # if sunday is holiday else go school 
+# if (today != "sunday") {
+#     echo("goto school")
+# }
+
+# else{
+
+#     echo("Today is holiday")
+
+#     }
+
+# if 20 is greater than > 100 then print, given number is > 20 else print lessthan 100
+
+NUM1=$N1
+
+if [$NUM1 gt 100]
+then
+   echo("Given number: $NUM1 is greater than 100")
+else 
+   echo("Given number: $NUM1 is less than 100")
+fi
---
+++ install-sql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM1=$1
+NUM2=$2
+
+DATE=$(date)
+
+if($NUM1 gt $NUM2){
+
+  
+
+}
